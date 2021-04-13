44d352369aae1b8e8d3fea9e55c1496ed5f733a0
Rugnus <sungur.gasanov@mail.ru>
2021-04-13 16:28:34 +0300

    Add slider to room section

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 let check = document.getElementById('menu-btn');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
 
 check.onclick = function() {
     var header = document.getElementById('header');
     header.classList.add('white')
+};
+
+next.onclick = function(e) {
+    var item = document.getElementsByClassName('room__item');
+    console.log(item);
+    item[0].classList.add('item-hide');
+    item[3].classList.remove('item-hide');   
+    item[3].classList.add('room__item');   
 }
 
-if (check.checked == true ) {
-    header.classList.add('white')
+prev.onclick = function(e) {
+    var item = document.getElementsByClassName('room__item');
+    console.log(item);
+    item[3].classList.add('item-hide');
+    item[0].classList.remove('item-hide');  
 }
+
+
+
+// next.addEventListener("click", function(e) {
+//     var item = document.getElementsByClassName('room__item');
+//     console.log(item);
+//     item[0].classList.add('item-hide');
+//     item[3].classList.add('item-show');   
+// });
